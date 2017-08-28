41f8db10e435546eb5db3aedf1180f62aee8bda0
sham <developer.shambhu@gmail.com>
2017-08-29 00:37:08 +0530

    server module

--- server.js
+++ server.js
@@ -3,13 +3,56 @@ var net = require('net');
 var util = require('util');
 var uuid = require('uuid');
 var events = require('events');
-var logger = require('mylogger');
+function mySock(socket) {
+    events.EventEmitter.call(this);
+    var self = this;
+    this.socket = socket;
+    this.socket.on('data', function (data) {
+        try {
+            self.parseResponse(JSON.parse(data));
+        } catch (e) {
+            throw e;
+        }
+    });
+    this.socket.on('end', function () {
+        self.emit('end');
+    });
+    this.socket.on('error', function (err) {
+        self.emit('error', err);
+    })
+}
+util.inherits(mySock, events.EventEmitter);
+mySock.prototype.parseResponse = function (data) {
+    var self = this;
+    if (data.type === 'ack') {
+        return this.emit('ack_' + data.id, data.data);
+    }
+    if (data.type === 'data') {
+        this.emit('data', data.data, function (data1) {
+            self._write({type: 'ack', id: data.id, data: data1});
+        });
+    }
+};
+mySock.prototype.send = function (data, cb) {
+    if (typeof cb !== 'function') {
+        return this._write(data);
+    }
+    var sendId = uuid();
+    this.on('ack_' + sendId, cb);
+    this._write({type: 'data', id: sendId, data: data});
+};
+mySock.prototype._write = function (data, cb) {
+    cb = cb || function () {};
+    var strData = (typeof data === 'string') ? data : JSON.stringify(data);
+    this.socket.write(strData, cb);
+}
 function ipcServer(sockPath) {
     this.sockPath = sockPath;
+    events.EventEmitter.call(this);
 }
+util.inherits(ipcServer, events.EventEmitter);
 ipcServer.prototype.createServer = function (sockPath, cb) {
     var self = this;
-    events.EventEmitter.call(this);
     if (typeof sockPath === 'string') {
         this.sockPath = sockPath;
     } else if (typeof sockPath === 'function') {
@@ -23,51 +66,11 @@ ipcServer.prototype.createServer = function (sockPath, cb) {
         }
     }
     net.createServer(function (socket) {
-        self.emit('connect', socket);
-        socket.on('data', function (data) {
-            try {
-                self.parseResponse(JSON.parse(data), socket);
-            } catch (e) {
-                throw e;
-            }
-        });
+        var mysock = new mySock(socket);
+        self.emit('connect', mysock);
     }).listen(this.sockPath, function (err) {
         cb(err);
     });
 };
-util.inherits(ipcServer, events.EventEmitter);
-ipcServer.prototype.parseResponse = function (socket, data) {
-    var self = this;
-    if (data.type === 'ack') {
-        return socket.emit('ack_' + data.id, data.data);
-    }
-    if (data.type === 'data') {
-        socket.emit('data', data.data, function (data1) {
-            self._write({type: 'ack', id: data.id, data: data1});
-        });
-    }
-};
-ipcServer.prototype.send = function (socket, data, cb) {
-    var sendId = uuid();
-    var strData = (typeof data === 'string') ? data : JSON.stringify(data);
-    if (typeof cb !== 'function') {
-        return socket.write(data);
-    }
-    this.on('ack_' + sendId, cb);
-    socket.write({type: 'message', id: sendId, data: data});
-};
 
-var server = new ipcServer();
-server.createServer('/tmp/sham1.sock', function () {
-    logger.info('server is listening on /tmp/sham1.sock');
-    setInterval(function () {
-        server.send('hello ', function (st) {
-            logger.log('data sent to server', st);
-        });
-    }, 1000)
-    server.on('data', function (data, cb, cb1) {
-        logger.log('sending ack for received data :', data);
-        cb({s: "received:" + Date.now()});
-    })
-});
- 
+module.exports = ipcServer;
